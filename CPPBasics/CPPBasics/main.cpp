ce7b6107eb52ad4be1f475a71f05784cd0f02039
SonghaCH <cwithus0002@gmail.com>
2026-03-26 12:25:03 +0900

    [Feat] 랜덤 숫자 맞추기 '행운의 잭팟' 게임 로직 구현
    입출력의 활용과 연산자,제어문을 다뤄보기 위해서 1부터 20까지의 뽑기 게임을 만들어 보았다. 정해진 결과로 하다보니 다시 하는 재미가 없어져서
    srand와 time 함수를 활용하여 매 게임마다 새로운 정답(1~20) 생성하여 재미를 주었고,
    만일 유저 입장에서 억울한 입장이 나오면 어떡하지? 라는 접근으로
    1~20 범위를 벗어난 입력 시 코인을 차감하지 않고 재입력을 유도하는 무한 루프 검증 로직을 구현하여 예외 처리도 해보았다. 아직은 랜덤을 다루기에는 부족하여서 AI 제미나이의 도움을 받아 게임을 완성하였다.
    한가지 아쉬운 점이 있다면, 힌트 시스템과 중복 선택을 하였을 때, 소거되는 효과도 넣었으면 좋겠다고 생각이 들어 추후 기회가 된다면 업데이트 할 예정이다.

--- CPPBasics/CPPBasics/main.cpp
+++ CPPBasics/CPPBasics/main.cpp
@@ -1,34 +1,113 @@
 ﻿#include <iostream>
-#include <string>
+#include <cstdlib>     //랜덤함수마법사
+#include <ctime>       //랜덤함수마법사
 
-bool isOpenRamen(int RamenCount)
-{
-    bool StartCook = (RamenCount == 1) ? true : false;
-    std::cout << "라면 개수 1개 확인 완료. 라면 요리 시작하겠습니다" << StartCook << std::endl;
-    return StartCook;
-}
-
-int CookRamen()
-{
-
-    std::cout << "면과 스프를 넣고 보글보글 끓입니다." << std::endl;
-    int CookTime = 180;
-    return CookTime;
-
-}
-
-void EatRamen(int CookTime)
-{
-    std::cout << CookTime << " 초 동안 맛있게 끓였습니다! 맛있게 드십쇼!" << std::endl;
-}
-
-
-
+using namespace std;
 
 int main()
 {
-    bool myRamen = isOpenRamen(1);
-    int time = CookRamen();
-    EatRamen(time);
-    return 0;
+    srand(time(NULL));    //랜덤함수
+    int JackPot = (rand() % 20) + 1;    //랜덤함수
+
+    int CountCoin = 10;
+    int ChoiceNumber;
+    int EnterButton;
+
+    cout << "*반갑습니다. 행운의 뽑기 시간입니다.*" << endl;
+    cout << "\n코인은 총 10개이며 10번안에 맞추지 못할시 게임오버 됩니다." << endl;
+    cout << "\n1부터 20까지 숫자이며 정답은 단 1개입니다." << endl;
+
+
+    for (;;)  //0또는 1번을 눌렀을때, 다시 시도하는 반복문
+    {
+
+        cout << "<진행하려면 (1)번을, 종료 하시려면 (0)번을 눌러주세요.>\n" << endl;
+
+
+        cin >> EnterButton;
+        if (EnterButton == 1)
+        {
+            cout << "게임을 시작하겠습니다!" << endl;
+            break;
+        }
+        else if (EnterButton == 0)
+        {
+            cout << "종료 버튼을 누르셨습니다! \n게임이 종료 됩니다." << endl;
+            exit;
+        }
+
+        else
+        {
+            cout << "\n잘못 입력하셨습니다. 1번 혹은 0번을 입력해주세요" << endl;
+        }
+
+    }
+
+    cout << "1부터 20까지 입력하여 당첨을 찾아십시오." << endl;
+
+    for (int i = 1; i <= CountCoin; i++)
+    {
+        for (; ; ) //1부터 20사이만 입력하도록 반복
+        {
+
+            cin >> ChoiceNumber;
+            if (ChoiceNumber >= 1 && ChoiceNumber <= 20)  //정상 입력시 진행
+            {
+                break;
+            }
+
+            else
+            {
+                cout << "\n1부터 20사이만 입력해주세요.(코인은 차감되지않습니다.)" << endl;
+            }
+        }
+
+        if (ChoiceNumber == JackPot)
+        {
+            cout << "\n7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7\n잭팟! 정답입니다! 상품은 주강사님의 *격려* 입니다\n축하드립니다!" << endl;
+            cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" << endl;
+            return 0;
+        }
+        else
+        {
+            cout << "\n아쉽게도 정답이 아닙니다" << endl;
+            cout << "남은 코인:  " << CountCoin - i << " 개 남았습니다. 상품이 매우 값진 보상이니 화이팅!" << endl;
+
+            if (i == CountCoin)
+            {
+                cout << "\n\n\n\n\n모든 코인을 소진하셨습니다. \nG  a   m  e       O  v  e   r" << endl;
+                cout << "\n\n\n\n<<아쉽게도 정답은 [" << JackPot << "] 이었습니다.>>" << endl;
+
+            }
+        }
+
+
+
+
+
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
