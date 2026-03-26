67b262dd51e2ab4020073ab9c827096d57aca06c
SonghaCH <cwithus0002@gmail.com>
2026-03-26 12:23:40 +0900

    [init] C++ 기초 학습 시작
    C++ main 함수의 구조를 파악하고, 이를 라면 기계를 제작하였다.
    함수의 첫 발자국이었다.

---
+++ CPPBasics/CPPBasics/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+bool isOpenRamen(int RamenCount)
+{
+    bool StartCook = (RamenCount == 1) ? true : false;
+    std::cout << "라면 개수 1개 확인 완료. 라면 요리 시작하겠습니다" << StartCook << std::endl;
+    return StartCook;
+}
+
+int CookRamen()
+{
+
+    std::cout << "면과 스프를 넣고 보글보글 끓입니다." << std::endl;
+    int CookTime = 180;
+    return CookTime;
+
+}
+
+void EatRamen(int CookTime)
+{
+    std::cout << CookTime << " 초 동안 맛있게 끓였습니다! 맛있게 드십쇼!" << std::endl;
+}
+
+
+
+
+int main()
+{
+    bool myRamen = isOpenRamen(1);
+    int time = CookRamen();
+    EatRamen(time);
+    return 0;
+}
