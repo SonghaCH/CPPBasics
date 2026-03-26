f469664ca81bd1f980b90f6cd3c5fc27e3e9a2e0
SonghaCH <cwithus0002@gmail.com>
2026-03-26 18:03:07 +0900

    [feat] 객체의 특징과 상속을 이용한 스타크래프트 3파전 만들기
    객체의 특성과 class 를 접하게 되어 그를 이용한 스타크래프트 3파전을 만들었다.
    생각보다 랜덤을 다루는것이 쉽지않고, AI 의 도움 없이는 코드를 완성하기 쉽지 않았다. 그러나 클래스 상속 개념과 생성자를 만들어 봄으로서 한 발 더 앞으로 나가는계기 되었다.

--- CPPBasics/CPPBasics/main.cpp
+++ CPPBasics/CPPBasics/main.cpp
@@ -1,143 +1,218 @@
 ﻿#include <iostream>
 #include <string>
+#include <ctime>        //랜덤함수를 위해 헤더 추가
+#include <cstdlib>      //  ==
 using namespace std;
 
-int liveMap[7][7] = { 0 };  //실시간 맵을 위한 전역 변수 모든 맵을 0(빈칸)으로 채우기
+class Unit
+{
+public:
 
-int main() {
+	string m_UnitName;
+	int m_hp;
+	int m_power;
+	int m_speed;
 
-    for (;;) //게임 오버시에는 다시 돌아와 클리어 할 수 있게 무한 루프
-    {
-        // 7행 7열 보물 맵 구현 (총 49칸)
-        // 아이템 ID: 0(빈칸), 1(보물), 2(몬스터)
-        int treasure[7][7] =
-        {
-            {0, 2, 0, 0, 2, 0, 1},
-            {0, 0, 0, 0, 2, 0, 0},
-            {0, 2, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 1},
-            {2, 0, 2, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 2, 0},
-            {0, 2, 0, 2, 0, 0, 1}
 
-        };
+	Unit(string name, int hp, int power, int speed) //생성자를 통해 하나 하나 대입하는 수고를 덜기
+		: m_UnitName(name), m_hp(hp), m_power(power), m_speed(speed)
+	{
 
-        int pX = 0;             //플레이어 위치가 0,0에서 시작을 의미
-        int pY = 0;
-        int findTreasure = 0;   // 보물을 찾은 개수 0부터 시작
+	}
+	virtual void attack(Unit& target)  //AI가 마린의 특수 공격을 위해 추가 권장								
+	{                                  //컴퓨터가 일반 유닛으로 인지하여 
+		cout << "\n\n <" << m_UnitName << "> 이 <" << target.m_UnitName << "> 을 공격!" << endl;
+		target.takeDamage(m_power);
+	}
 
-        bool isGameover = false; //AI왈 for문을 진행하다가 게임 오버가 되었을때, 재시작을 위한 컴퓨터에게 메모지 역할
 
-        cout << "\n[트레저 헌터 게임을 시작합니다]\n" << endl;
-        cout << "\n[   조작법 -      1번 왼쪽,  2번 윗쪽,  3번 아래쪽,  4번 오른쪽    ]\n" << endl;
-
-        for (; findTreasure < 3;)   //AI왈 보물 3개를 다 찾을 때까지 플레이해야함.
-        {
-
-            system("cls");   //게임 빌드 화면이 너무 더러워져서 바로바로 로그만 확인하고 사라지는 명령어 AI 300퍼센트 참고
-
-            cout << "\n[트레저 헌터 게임 진행 중]" << endl;
-            cout << "[조작법 - 1:왼쪽, 2:위쪽, 3:아래쪽, 4:오른쪽]" << endl;
-            cout << "------------------------------------------" << endl;
-
-            cout << "\n----------------" << endl;
-            for (int i = 0; i < 7; i++)       //AI 100퍼센트 지분 실시간 지도 출력을 위해서 이중 for문을 사용함.
-            {
-                for (int r = 0; r < 7; r++)
-                {
-                    if (i == pY && r == pX)
-                    {
-                        cout << "P ";        // 현재 플레이어 위치
-                    }
-                    else if (liveMap[i][r] == 1)
-                    {
-                        cout << "O ";  //실시간 지도에 보물(1) 위치 표기
-                    }
-                    else if (liveMap[i][r] == 2)
-                    {
-                        cout << "X "; //실시간 지도에 괴물(2) 위치 표기
-                    }
-                    else
-                    {
-                        cout << ". ";   //실시간 지도에 빈칸(0) 표기
-                    }
-
-                }
-                cout << endl;      //매우 중요! 실시간 지도 만드는데 표가 1자로 내려가지 않은 이유 고맙다 AI..
-            }
+	virtual void takeDamage(int damage)
+	{
+		m_hp -= damage;
+	}
 
 
 
 
-            cout << "\n\n\n\n트레저 헌터 출동!   " << findTreasure << "/3 만큼 보물 획득했습니다.  |  현재위치: (" << pX << "." << pY << ")" << endl; //게임 시작선언과 보물 3가지중 n개 찾았다는 표기, 그리고 현재 위치는 x y 좌표 표기
+};
 
-            int move;                                      //이동방향 유저가 입력 
-            cout << "\n어느 쪽으로 이동할까요? : ";
-            cin >> move;
+class Marine : public Unit
+{
+public:
+	Marine() : Unit("Marine", 100, 10, 15)    //부모 생성자를 기반으로 마린 생성자 작성
+	{
 
-            int nextX = pX;            //플레이어의 실질적 좌표 선언
-            int nextY = pY;
+	}
+	
+	void attack(Unit& target) override     //부모 클래스의 공격 기반이지만 마린 만의 특징을 작성
+	{									  
+		Unit::attack(target);
+		cout << "  [ You wanna piece of me, boy? ]  마린의 점점 강력해지는 공격" << endl;
 
-            if (move == 1) nextX--;          //방향 번호 입력 어차피 왼쪽 오른쪽은 nextX에서 증감, 위 아래는 nextY에서 증감
-            else if (move == 2) nextY--;
-            else if (move == 3) nextY++;
-            else if (move == 4) nextX++;
-            else
-            {
-                cout << "잘못 입력하셨습니다. 1,2,3,4 번 중에서 선택하여 이동하여 주시기 바랍니다." << endl;
-                system("pause");
-                continue;
-            }
+		m_power += 3;                    //스팀팩 스킬 구현  때릴때마다 공격력 3이 올라가지만
+		if (m_hp > 1)                    // HP 1을 잃는다. 하지만 피가 1보다 많을때만 적용 된다.
+		{
+			m_hp -= 1;
+			cout << " 마린이 공격마다 스팀팩을 주사합니다! 공격력이 " << m_power << "로 상승하지만 1씩 체력이 깎입니다! " << endl;
+		}
+		
+	}
 
-            if (nextX < 0 || nextX > 6 || nextY < 0 || nextY > 6)      //7X7 맵이기 때문에 밖으로 나가는것을 방지
-            {
+};
 
-                cout << "벽에 막혔습니다. 다른 방향으로 이동하여 주시기 바랍니다." << endl;
-                system("pause"); // 로그만 사라질 시간 확보하기 사라지는 명령어 AI 300퍼센트 참고
-                continue;
+class Zergling : public Unit
+{
+public:
+	Zergling() : Unit("Zergling", 40 * 2, 5 * 2, 20)    //부모 클래스로 저글링 생성자(사실 그냥 연산하여 기입해도 되지만 2마리 라는것을 강조)
+	{
 
+	}
 
-            }
-
-            pX = nextX;       //AI 피셜 안전함이 확인되면 좌표 확정 (벽에 부딪히거나 옳지 않은 선택지를 고르는것을 뜻하는것으로 보임)
-            pY = nextY;
-
-            if (treasure[pY][pX] == 1)
-            {
-                cout << "!!!!!!!!!!!!!보물을 발견 하셨군요!!!!!!!!!!!!!!" << endl;
-                system("pause");
-                liveMap[pY][pX] = 1;       //실시간 지도에 보물 위치 기록
-                treasure[pY][pX] = 0;     //한번 발견했으니 정답지에서 제거 합니다.보물(1)을 빈칸(0) 으로 변경
-                findTreasure++; //  n/3 개로 3개중에 1개 찾았으니 증가 시켜줍니다 
-            }
-            else if (treasure[pY][pX] == 2)
-            {
-                cout << "XXXXXXXXX 매복해있던 몬스터에게 당했습니다! 걱정마세요! 다시 시작합니다. XXXXXXXXXXXXX" << endl;
-                liveMap[pY][pX] = 2;   //실시간 위치에 몬스터 위치 기록
-
-                system("pause");
-                isGameover = true;          //처음에 선언했던 isGameover = false 가 조건에 의해 게임오버 true 로 변경됩니다.
-                break;
-            }
-            else
-            {
-                cout << "이 곳은 안전하군요. 위험이 도사리고 있습니다. 조심하세요! " << endl;
-                system("pause");
-            }
+	void attack(Unit& target) override
+	{
+		Unit::attack(target);
+		cout << " 저글링 2마리가 맹렬하게 달려들어 " << target.m_UnitName << " 의 발목을 물어뜯습니다! " << endl;
+	}
 
 
-        }
-        if (isGameover == false)
-        {
-            cout << "\n\n축하드립니다! 모든 보물을 찾으셨습니다. 당신은 진정한 트레져헌터!\n\n" << endl;
-            system("pause");
-            break;
-        }
+};
 
-    }
-    return 0;
-}
+class Zealot : public Unit
+{
+public:
+	int m_shield = 30;
+	int m_attackTimer = 0;
 
+	Zealot() : Unit("Zealot", 100, 16, 12) //질럿 생성자
+	{
+		
+	}
+
+	void attack(Unit& target) override
+	{
+		if (m_attackTimer == 0)
+		{
+			Unit::attack(target);
+			m_attackTimer = 1;
+			cout << "[ My life for Aiur! ] 질럿의 강력한 충전 공격!\n\n" << endl;
+		}
+		else
+		{
+			m_attackTimer = 0;    // 쉬었으니 다음 턴엔 공격 가능
+			cout << "\n\n질럿 공격 충전중...\n\n" << endl;
+		}
+	}
+
+	void takeDamage(int damage) override     //내구력이 있는 질럿 보호막 추가
+	{
+		if (m_shield > 0)
+		{
+			m_shield -= damage;
+			cout << " 질럿 보호막 작동! (남은 보호막: " << (m_shield > 0 ? m_shield : 0) << ")" << endl;
+
+			if (m_shield < 0)
+			{
+				m_hp += m_shield;
+				m_shield = 0;
+			}
+
+		}
+		else
+			Unit::takeDamage(damage);
+	}
+};
+
+
+	int main()
+	{
+		srand((unsigned int)time(NULL));     //AI 참조 하여 랜덤문 추가
+		Marine m;                            //각 유닛들 소환
+		Zergling z;
+		Zealot ze;
+
+		cout << "--- 종족별 3파전 대난투 시작! ---" << endl;
+		cout << "----------------------------------" << endl;
+
+		int turn = 1;
+
+		while (true)
+
+		{
+			system("cls");
+			cout << "\n[TURN " << turn << "]" << endl;
+			cout << "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" << endl;
+			cout << "  마린 HP: " << m.m_hp << " | 저글링 HP: " << z.m_hp << " | 질럿 HP: " << ze.m_hp << " (S: " << ze.m_shield << ")" << endl;
+			cout << "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" << endl;
+			bool mAlive = m.m_hp > 0;                       //1명 남을때까지 싸우기
+			bool zAlive = z.m_hp > 0;
+			bool zeAlive = ze.m_hp > 0;
+
+
+
+
+			int aliveCount = (int)mAlive + (int)zAlive + (int)zeAlive;           //살아있는 유닛 1개 이하면 종료
+			if (aliveCount <= 1) break;
+
+			cout << "\n[TURN " << turn << "]" << endl;
+
+			if (mAlive)
+			{
+				if (zAlive && zeAlive)
+				{
+					(rand() % 2 == 0) ? m.attack(z) : m.attack(ze);    //랜덤으로 2중 1명 때리기
+				}
+				else if (zAlive) {
+					m.attack(z);
+				}
+				else if (zeAlive) {
+					m.attack(ze);
+				}
+			}
+
+			if (zAlive)
+			{
+				if (mAlive && zeAlive)
+				{
+					(rand() % 2 == 0) ? z.attack(m) : z.attack(ze);
+				}
+				else if (mAlive)
+				{
+					z.attack(m);
+				}
+				else if (zeAlive)
+				{
+					z.attack(ze);
+				}
+			}
+
+			if (zeAlive)
+			{
+				if (mAlive && zAlive)
+				{
+					(rand() % 2 == 0) ? ze.attack(m) : ze.attack(z);
+				}
+				else if (mAlive)
+				{
+					ze.attack(m);
+				}
+				else if (zAlive)
+				{
+					ze.attack(z);
+				}
+			}
+
+			turn++;        //빨리 지나가기 방지
+			system("pause");
+		}
+		cout << "\n======================================" << endl;
+		if (m.m_hp > 0) cout << " 최종 승자: 테란 마린!!" << endl;
+		else if (z.m_hp > 0) cout << " 최종 승자: 저그 저글링!!" << endl;
+		else if (ze.m_hp > 0) cout << " 최종 승자: 프로토스 질럿!!" << endl;
+		else cout << " 모두가 전사했습니다... 무승부!" << endl;
+		cout << "======================================" << endl;
+
+		return 0;
+	}
 
 
 
